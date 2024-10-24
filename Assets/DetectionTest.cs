3ad050daa94568c8098b00280a783ffe539c51f5
Christoph Fleschutz <ch.fleschutz@gmail.com>
2024-10-24 17:41:09 +0200

    Add Bucket to tracking

--- Assets/DetectionTest.cs
+++ Assets/DetectionTest.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Linq;
 using UI = UnityEngine.UI;
 using Klak.TestTools;
+using System.Collections.Generic;
 
 sealed class DetectionTest : MonoBehaviour
 {
@@ -11,6 +12,9 @@ sealed class DetectionTest : MonoBehaviour
     [SerializeField] Material _tagMaterial = null;
     [SerializeField] UI.RawImage _webcamPreview = null;
     [SerializeField] UI.Text _debugText = null;
+
+    [SerializeField] GameObject _tagPrefab = null;
+    Dictionary<int, GameObject> _tagObjects = new Dictionary<int, GameObject>();
 
     AprilTag.TagDetector _detector;
     TagDrawer _drawer;
@@ -40,9 +44,24 @@ sealed class DetectionTest : MonoBehaviour
         var fov = Camera.main.fieldOfView * Mathf.Deg2Rad;
         _detector.ProcessImage(image, fov, _tagSize);
 
+        // Start with all tag objects disabled
+        foreach (var obj in _tagObjects.Values)
+            obj.SetActive(false);
+
         // Detected tag visualization
         foreach (var tag in _detector.DetectedTags)
+        {
+            if (!_tagObjects.ContainsKey(tag.ID))
+                _tagObjects[tag.ID] = Instantiate(_tagPrefab);
+
+            var obj = _tagObjects[tag.ID];
+            obj.transform.position = tag.Position;
+            obj.transform.rotation = tag.Rotation;
+            obj.transform.localScale = Vector3.one * _tagSize;
+            obj.SetActive(true);
+
             _drawer.Draw(tag.ID, tag.Position, tag.Rotation, _tagSize);
+        }
 
         // Profile data output (with 30 frame interval)
         if (Time.frameCount % 30 == 0)
